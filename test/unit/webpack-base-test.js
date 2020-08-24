2f5ba2ace3ca66c36f8b8e2e839f1d79674ad3cc
JasonPing <eazyee6@gmail.com>
2020-08-24 20:25:59 +1000

    change assert

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -1,18 +1,15 @@
-const assert = require('assert');
-const { entry } = require('../../lib/webpack.base.js');
+const assert = require("assert");
+const path = require("path");
+const projectRoot = process.cwd();
 
-describe('webpack.base.js test case', () => {
-  const baseConfig = require('../../lib/webpack.base.js');
+const { entry } = require("../../lib/webpack.base.js");
+
+describe("webpack.base.js test case", () => {
+  const baseConfig = require("../../lib/webpack.base.js");
 
   console.log(baseConfig);
-  it('entry', () => {
-    assert.equal(
-      baseConfig.entry.index,
-      'D:/coding/webpack/my-project/builder-webpack/test/smoke/template/src/index/index.js'
-    );
-    assert.equal(
-      baseConfig.entry.search,
-      'D:/coding/webpack/my-project/builder-webpack/test/smoke/template/src/search/index.js'
-    );
+  it("entry", () => {
+    assert.equal(baseConfig.plugins[1].cleanStaleWebpackAssets, true);
+    assert.equal(baseConfig.plugins[1].protectWebpackAssets, true);
   });
 });
